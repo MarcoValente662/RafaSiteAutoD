eec57bf44f5e27016ec8c0e509915960f7e054ca
MarcoValenteF <mafigueira@deloitte.pt>
2026-06-09 11:28:46 +0100

    x

--- backend/server.py
+++ backend/server.py
@@ -10,7 +10,6 @@ from typing import List
 import uuid
 from datetime import datetime, timezone
 
-
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
@@ -24,7 +23,6 @@ app = FastAPI()
 
 # Create a router with the /api prefix
 api_router = APIRouter(prefix="/api")
-
 
 # Define Models
 class StatusCheck(BaseModel):
